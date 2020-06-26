98ca8d6e6d4efd17e86fc7b426a8a81ff5b3b2e1
integerJI <jjs9536@gmail.com>
2020-06-26 18:35:48 +0900

    모델만들기

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Report(models.Model):
+    objects = models.Manager()
+    input_user = models.ForeignKey(User, on_delete = models.CASCADE)
+    input_date = models.DateTimeField('date published', default=timezone.now)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 # app/view.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.utils import timezone
+from .models import Report
 
 # Create your views here.
 
 def index(request):
+    report = Report.objects.all()
+
+    if request.method == "POST":
+        report.body = request.POST['body']
+        report.pub_date = timezone.datetime.now()
+        report.save()
+        return redirect(reverse('index'))
+
+    else:
+        return render(request, 'index.html')
+
     return render(request, 'index.html')
