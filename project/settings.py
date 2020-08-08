debaf3882d94ac476c12a734438dfac6ad410f8a
integerJI <jjs9536@gmail.com>
2020-08-08 22:00:58 +0900

    base.html 생성

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['project/base.html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
