54aa350e97b278bcb69c05e409f93a47de09a239
integerJI <jjs9536@gmail.com>
2020-08-11 20:37:16 +0900

    ajax 호출중중

--- user/views.py
+++ user/views.py
@@ -21,21 +21,26 @@ def signup(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
+        print(username,'/',password1,'/',password2)
+
         if not username:
             message = '아이디를 입력해주세요. error (01)'
+            error = '00'
             print('01')
-            return render(request, 'signup.html', {'message' : message})
+            return render(request, 'signup.html', {'message' : message,'error':error})
 
         if not password1:
             message = '비밀번호를 입력해주세요. error (02)'
+            error = '00'
             print('02')
-            return render(request, 'signup.html', {'message' : message})
+            return render(request, 'signup.html', {'message' : message,'error':error})
 
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 message = '아이디가 중복됩니다. 다시 시도해주세요. error (03)'
+                error = '00'
                 print('03')
-                return render(request, 'signup.html', {'message' : message})
+                return render(request, 'signup.html', {'message' : message,'error':error})
             else:
                 user = User.objects.create_user(username, password=password1)
                 print('04')
@@ -43,17 +48,18 @@ def signup(request):
                 return redirect('index')
         else:
             message = '비밀번호가 다릅니다. 다시 시도해주세요. error (04)'
+            error = '00'
             print('05')
-            return render(request, 'signup.html', {'message' : message})
+            return render(request, 'signup.html', {'message' : message,'error':error})
         
     return render(request, 'signup.html')
 
-# class Loginviews(LoginView):
-#     template_name = 'signin.html'
-#     def form_invalid(self, form):
-#         messages.error(self.request, '로그인에 실패하였습니다. Id 혹은 Password를 확인해 주세요.', extra_tags='danger')
-#         return super().form_invalid(form)
-# signin = Loginviews.as_view()
+class Loginviews(LoginView):
+    template_name = 'signin.html'
+    def form_invalid(self, form):
+        messages.error(self.request, '로그인에 실패하였습니다. Id 혹은 Password를 확인해 주세요.', extra_tags='danger')
+        return super().form_invalid(form)
+signin = Loginviews.as_view()
 
 # def signin(request):
 #     if request.method == 'POST':
@@ -78,25 +84,25 @@ def signup(request):
 #     else:
 #         return render(request, 'signin.html')
 
-def signin(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+# def signin(request):
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         password = request.POST['password']
         
-        if not username:
-            message = '아이디를 입력해주세요. error (05)'
-            return render(request, 'signin.html', {'message' : message})
+#         if not username:
+#             message = '아이디를 입력해주세요. error (05)'
+#             return render(request, 'signin.html', {'message' : message})
 
-        if not password:
-            message = '비밀번호를 입력해주세요. error (06)'
-            return render(request, 'signin.html', {'message' : message})
+#         if not password:
+#             message = '비밀번호를 입력해주세요. error (06)'
+#             return render(request, 'signin.html', {'message' : message})
 
-        user = authenticate(username = username, password = password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-    else:
-        return render(request, 'signin.html')
+#         user = authenticate(username = username, password = password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('index')
+#     else:
+#         return render(request, 'signin.html')
 
 class LogoutViews(LogoutView):
     next_page = settings.LOGOUT_REDIRECT_URL
