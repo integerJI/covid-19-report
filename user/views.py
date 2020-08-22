c115ca145ac28c80004fa3fbe3791435674ef31b
integerJI <jjs9536@gmail.com>
2020-08-22 23:21:21 +0900

    블로그 올리기용 다시 적기

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.views.generic.edit import CreateView
@@ -65,3 +65,30 @@ def signin(request):
 class LogoutViews(LogoutView):
     next_page = settings.LOGOUT_REDIRECT_URL
 signout = LogoutViews.as_view()
+
+class UserPasswordResetView(PasswordResetView):
+    template_name = 'password_reset.html' #템플릿을 변경하려면 이와같은 형식으로 입력
+    success_url = reverse_lazy('password_reset_done')
+    
+    def form_valid(self, form):
+        print("form_valid 진입")
+        if User.objects.filter(email=self.request.POST.get("email")).exists():
+            opts = {
+                'use_https': self.request.is_secure(),
+                'token_generator': self.token_generator,
+                'from_email': self.from_email,
+                'email_template_name': self.email_template_name,
+                'subject_template_name': self.subject_template_name,
+                'request': self.request,
+                'html_email_template_name': self.html_email_template_name,
+                'extra_email_context': self.extra_email_context,
+            }
+            form.save(**opts)
+            print("form_save")
+            return super().form_valid(form)
+        else:
+            print("메일 전송 실패")
+            return render(self.request, 'password_reset_done_fail.html')
+
+class UserPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'password_reset_done.html' #템플릿을 변경하려면 이와같은 형식으로 입력
