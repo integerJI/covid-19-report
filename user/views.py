06b690623b8200da4f2dbad8302eac449c4b10a2
integerJI <jjs9536@gmail.com>
2020-08-24 23:42:52 +0900

    del -> pop

--- user/views.py
+++ user/views.py
@@ -173,7 +173,7 @@ class UserPasswordResetConfirmView(PasswordResetConfirmView):
 
     def form_valid(self, form):
         user = form.save()
-        del self.request.session[INTERNAL_RESET_SESSION_TOKEN]
+        self.request.session.pop(INTERNAL_RESET_SESSION_TOKEN, None)
         if self.post_reset_login:
             auth_login(self.request, user, self.post_reset_login_backend)
         return super().form_valid(form)
