7a1cec9271460be352a9a06f504e6b93dbf46103
integerJI <jjs9536@gmail.com>
2020-08-25 23:55:53 +0900

    실패 후 원복

--- user/urls.py
+++ user/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     path('password_reset/', views.UserPasswordResetView.as_view(), name="password_reset"),
     path('password_reset_done/', views.UserPasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('password_reset_confirm/<slug:uidb64>/<slug:token>/', views.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password_reset_confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     # path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),	
     path('password_reset_complete/', views.UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
