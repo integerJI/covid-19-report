9f2a07b2b4d39f8a473fa1489f05f2e4ce4cf54d
integerJI <jjs9536@gmail.com>
2020-08-25 23:48:38 +0900

    실패 후 원복복

--- user/views.py
+++ user/views.py
@@ -164,7 +164,7 @@ class UserPasswordResetConfirmView(PasswordResetConfirmView):
             print('get_user try')
             # urlsafe_base64_decode() decodes to bytestring
             # uid = urlsafe_base64_decode(uidb64).decode() 2020-08-25 django 2.0 + 부터는 uid에 decode 할 필요가 없어졌다고 해서 삭제
-            uid = urlsafe_base64_decode(uidb64)
+            uid = urlsafe_base64_decode(uidb64).decode()
             
             print(uid,'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
             user = UserModel._default_manager.get(pk=uid)
