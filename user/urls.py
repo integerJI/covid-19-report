23fc222384855def47651fe613ea66dc2f4c79ea
integerJI <jjs9536@gmail.com>
2020-08-25 23:34:25 +0900

    기존의 url과 비교

--- user/urls.py
+++ user/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('password_reset/', views.UserPasswordResetView.as_view(), name="password_reset"),
     path('password_reset_done/', views.UserPasswordResetDoneView.as_view(), name="password_reset_done"),
     path('password_reset_confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    # path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),	
     path('password_reset_complete/', views.UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
