870b1756aab2815a75e51a4fffc686063ea43012
integerJI <jjs9536@gmail.com>
2020-08-26 23:11:36 +0900

    url 수정 reset로

--- user/urls.py
+++ user/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     path('password_reset/', views.UserPasswordResetView.as_view(), name="password_reset"),
     path('password_reset_done/', views.UserPasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('password_reset_confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     # path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),	
-    path('password_reset_complete/', views.UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset/', views.UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
