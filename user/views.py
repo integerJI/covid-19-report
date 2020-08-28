94e545d8451e0939f1a72596a26fca90f92ae5a5
integerJI <jjs9536@gmail.com>
2020-08-28 21:41:12 +0900

    메일 두번가는거 수정

--- user/views.py
+++ user/views.py
@@ -92,17 +92,6 @@ class UserPasswordResetView(PasswordResetView):
     
     def form_valid(self, form):
         if User.objects.filter(email=self.request.POST.get("email")).exists():
-            opts = {
-                'use_https': self.request.is_secure(),
-                'token_generator': self.token_generator,
-                'from_email': self.from_email,
-                'email_template_name': self.email_template_name,
-                'subject_template_name': self.subject_template_name,
-                'request': self.request,
-                'html_email_template_name': self.html_email_template_name,
-                'extra_email_context': self.extra_email_context,
-            }
-            form.save(**opts)
             return super().form_valid(form)
         else:
             return render(self.request, 'password_reset_done_fail.html')
